469ec89b0870fc2fc2ea5638080c54cbfc4779f9
john-seredich <seredichjohnalt@gmail.com>
2023-03-01 13:11:02 -0500

    feat: sidebar component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "@tremor/react/dist/esm/tremor.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
