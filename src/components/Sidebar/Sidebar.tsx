22b4b69ed9eeffea93d8a42b4d2057888e67d58d
john-seredich <seredichjohnalt@gmail.com>
2023-03-02 16:01:38 -0500

    feat: dynamic styles for open and closed sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,17 +2,35 @@ import styles from "./Sidebar.module.scss";
 import { links } from "../../data/links";
 import { AiOutlineMenu } from "react-icons/ai";
 
-function Sidebar() {
+type Props = {
+  activeMenu: boolean;
+  setActiveMenu: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function Sidebar({ activeMenu, setActiveMenu }: Props) {
   const activeItem = "Dashboard";
+  const dynamicStyle = `${styles.sidebar} ${activeMenu}`;
+  console.log(dynamicStyle);
 
   const linksElement = links.map((link, i) => {
     return (
-      <div className={styles.sidebar__item} key={i}>
+      <div
+        className={
+          activeMenu ? styles.sidebar_open__item : styles.sidebar__item
+        }
+        key={i}
+      >
         {/* Render title only if it has a title */}
         {link.title.length > 1 && <h3>{link.title}</h3>}
 
         {/* Render each link to an a element */}
-        <div className={styles.sidebar__item__links}>
+        <div
+          className={
+            activeMenu
+              ? styles.sidebar_open__item__links
+              : styles.sidebar__item__links
+          }
+        >
           {link.links.map((linkItems, i) => {
             return (
               <a
@@ -30,14 +48,29 @@ function Sidebar() {
   });
 
   return (
-    <div className={styles.sidebar}>
-      <div className={styles.sidebar__header}>
+    <div className={activeMenu ? styles.sidebar_open : styles.sidebar}>
+      <div
+        className={
+          activeMenu ? styles.sidebar_open__header : styles.sidebar__header
+        }
+      >
         <h2>BITUMEN</h2>
-        <button type="button">
+        <button
+          type="button"
+          onClick={() => setActiveMenu((prevState) => !prevState)}
+        >
           <AiOutlineMenu />
         </button>
       </div>
-      <div className={styles.sidebar__container}>{linksElement}</div>
+      <div
+        className={
+          activeMenu
+            ? styles.sidebar_open__container
+            : styles.sidebar__container
+        }
+      >
+        {linksElement}
+      </div>
     </div>
   );
 }
