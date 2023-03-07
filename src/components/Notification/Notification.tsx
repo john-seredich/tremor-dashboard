5d2560b647920a86e5758ed2245e85d8966adde6
john-seredich <seredichjohnalt@gmail.com>
2023-03-07 14:10:11 -0500

    feat: map over notification data to dynamically render notification component

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,34 +1,27 @@
 import styles from "./Notification.module.scss";
-import profileImage from "../../assets/avatar.jpg";
-import profileImage2 from "../../assets/avatar2.jpg";
+import { notificationData } from "../../data/notifications";
 
 function Notification() {
+  const notificationElement = notificationData.map((data) => {
+    return (
+      <div className={styles.notification__item}>
+        <img src={data.profileImage} alt="" />
+        <div className={styles.notification__item__content}>
+          <p>{data.title}</p>
+          <p>{data.text}</p>
+        </div>
+        <p className={styles.notification__item_date}>{data.date}</p>
+      </div>
+    );
+  });
+
   return (
     <div className={styles.notification}>
       <div className={styles.notification__header}>
         <h2>Notification</h2>
         <p>3 New</p>
       </div>
-
-      {/* Item 1 */}
-      <div className={styles.notification__item}>
-        <img src={profileImage} alt="" />
-        <div className={styles.notification__item__content}>
-          <p>Congratulations John!</p>
-          <p>You have recieved a new follower.</p>
-        </div>
-        <p className={styles.notification__item_date}>Today</p>
-      </div>
-
-      {/* Item 2 */}
-      <div className={styles.notification__item}>
-        <img src={profileImage2} alt="" />
-        <div className={styles.notification__item__content}>
-          <p>Participate in a survey.</p>
-          <p>As a new user please fill out this...</p>
-        </div>
-        <p className={styles.notification__item_date}>Yesterday</p>
-      </div>
+      {notificationElement}
       <div className={styles.notification__read}>
         <a href="/">Read all notifications</a>
       </div>
---
+++ src/data/notifications.tsx
@@ -0,0 +1,17 @@
+import profileImage from "../assets/avatar.jpg";
+import profileImage2 from "../assets/avatar2.jpg";
+
+export const notificationData = [
+  {
+    profileImage,
+    title: "Congratulations John!",
+    text: "You have recieved a new follower.",
+    date: "Today",
+  },
+  {
+    profileImage: profileImage2,
+    title: "Participate in a survey.",
+    text: "As a new user please fill out this...",
+    date: "Yesterday",
+  },
+];
