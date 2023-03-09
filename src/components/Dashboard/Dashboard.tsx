ecfedaaada985ba2e626ac9e6abc240e3d0555b2
john-seredich <seredichjohnalt@gmail.com>
2023-03-09 16:36:33 -0500

    feat: basic dashboard styles and layout

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,12 @@
+import KpiCards from "../KpiCards/KpiCards";
 import styles from "./Dashboard.module.scss";
-import { Flex, Card, Text, Metric } from "@tremor/react";
 
 function Dashboard() {
   return (
     <div className={styles.dashboard}>
-      dashboard
-      {/* <Flex>
-        <Card maxWidth="max-w-xs" decoration="top" decorationColor="blue">
-          <Text>Sales</Text>
-          <Metric>$34,743</Metric>
-        </Card>
-      </Flex> */}
+      <div className={styles.dashboard__container}>
+        <KpiCards />
+      </div>
     </div>
   );
 }
