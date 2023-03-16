9d45dd24df1f8fc55f8de35b7c52b5f3603df3fe
john-seredich <seredichjohnalt@gmail.com>
2023-03-16 15:53:08 -0400

    feat: add functionality to all navbar buttons to handle open and close state

--- src/components/NewNavbar/NewNavbar.tsx
+++ src/components/NewNavbar/NewNavbar.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import styles from "./NewNavbar.module.scss";
-
 import { MdNotifications } from "react-icons/md";
 import { AiFillSetting, AiOutlineMenu } from "react-icons/ai";
 import { FiMoreVertical } from "react-icons/fi";
+import Notification from "../Notification/Notification";
+import ThemeModal from "../ThemeModal/ThemeModal";
+import Dropdown from "../Dropdown/Dropdown";
 
 type Props = {
   activeMenu: boolean;
@@ -10,12 +13,22 @@ type Props = {
 };
 
 function NewNavbar({ activeMenu, setActiveMenu }: Props) {
+  const [notifToggle, setNotifToggle] = useState(false);
+  const [themeModalToggle, setThemeModalToggle] = useState(false);
+  const [dropdownToggle, setDropdownToggle] = useState(true);
+
   const handleMenuToggle = () => setActiveMenu((prevMenu) => !prevMenu);
+  const handleNotifToggle = () => setNotifToggle((prevToggle) => !prevToggle);
+  const handleThemeModalToggle = () =>
+    setThemeModalToggle((prevToggle) => !prevToggle);
+  const handleDropdownToggle = () =>
+    setDropdownToggle((prevToggle) => !prevToggle);
 
   return (
     <>
       <div className={styles.navbar}>
         <div className={styles.navbar__container}>
+          {/* Menu */}
           <button
             className={styles.navbar__container__menubtn}
             type="button"
@@ -23,16 +36,39 @@ function NewNavbar({ activeMenu, setActiveMenu }: Props) {
           >
             <AiOutlineMenu />
           </button>
+
+          {/* Notification */}
           <div className={styles.navbar__menu}>
-            <button type="button" className={styles.navbar__menu__btn}>
+            <button
+              type="button"
+              className={styles.navbar__menu__btn}
+              onClick={handleNotifToggle}
+            >
               <MdNotifications />
             </button>
-            <button type="button" className={styles.navbar__menu__btn}>
+            {notifToggle && <Notification />}
+
+            {/* Settings */}
+            <button
+              type="button"
+              className={styles.navbar__menu__btn}
+              onClick={handleThemeModalToggle}
+            >
               <AiFillSetting />
             </button>
-            <button type="button" className={styles.navbar__menu__btn}>
+            {themeModalToggle && (
+              <ThemeModal setThemeModalToggle={setThemeModalToggle} />
+            )}
+
+            {/* Extends */}
+            <button
+              type="button"
+              className={styles.navbar__menu__btn}
+              onClick={handleDropdownToggle}
+            >
               <FiMoreVertical />
             </button>
+            {dropdownToggle && <Dropdown />}
           </div>
         </div>
       </div>
