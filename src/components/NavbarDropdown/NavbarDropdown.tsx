22497284f4aef1203e123217336bd7fa44a343fc
john-seredich <seredichjohnalt@gmail.com>
2023-03-24 13:24:31 -0400

    feat: profile Toggle for Navbar Dropdown for mobile

--- src/components/NavbarDropdown/NavbarDropdown.tsx
+++ src/components/NavbarDropdown/NavbarDropdown.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import styles from "./NavbarDropdown.module.scss";
 import { HiSearch } from "react-icons/hi";
 import { TextInput } from "@tremor/react";
 import profileImg from "../../assets/avatar.jpg";
+import ProfileDropdown from "../ProfileDropdown/ProfileDropdown";
 
 function NavbarDropdown() {
+  const [profileToggle, setProfileToggle] = useState(false);
+  const handleProfileToggle = () =>
+    setProfileToggle((prevToggle) => !prevToggle);
+
   return (
     <div className={styles.dropdown}>
       <div className={styles.dropdown__search}>
         <TextInput icon={HiSearch} placeholder="Search..." />
       </div>
-      <div className={styles.dropdown__profile}>
+      <button
+        type="button"
+        className={styles.dropdown__profile}
+        onClick={handleProfileToggle}
+      >
         <img src={profileImg} alt="" />
         <p>John</p>
-      </div>
+      </button>
+      {profileToggle && <ProfileDropdown />}
     </div>
   );
 }
