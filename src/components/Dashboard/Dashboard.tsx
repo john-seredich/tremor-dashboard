375b63c4516800344af1a5bab8b2eda7278e3a0b
john-seredich <seredichjohnalt@gmail.com>
2023-03-29 13:35:37 -0400

    feat: Add KPICards to dashboard component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,12 @@
+import KPICards from "../KPICards/KPICards";
 import styles from "./Dashboard.module.scss";
 
 function Dashboard() {
-  return <div></div>;
+  return (
+    <div className={styles.dashboard}>
+      <KPICards />
+    </div>
+  );
 }
 
 export default Dashboard;
