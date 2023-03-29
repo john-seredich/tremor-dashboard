6a45c038af1d6f699c78fc7f820d90d3e38d58b9
john-seredich <seredichjohnalt@gmail.com>
2023-03-29 13:34:17 -0400

    feat: Dynamic KPI Card component for dashboard

---
+++ src/components/KPICards/KPICards.tsx
@@ -0,0 +1,61 @@
+import styles from "./KPICards.module.scss";
+import {
+  Card,
+  Metric,
+  Text,
+  Flex,
+  BadgeDelta,
+  ColGrid,
+  Icon,
+} from "@tremor/react";
+
+import { KPICardData } from "./KPICardsData";
+import { KPIColors } from "./KPICardsData";
+
+function KPICards() {
+  return (
+    <div>
+      <ColGrid numColsSm={2} numColsLg={4} gapX="gap-x-6" gapY="gap-y-6">
+        {KPICardData.map((item) => (
+          <Card
+            key={item.title}
+            maxWidth="max-w-full"
+            decoration="top"
+            decorationColor={item.color}
+          >
+            <Flex alignItems="items-center" justifyContent="justify-start">
+              <Icon icon={item.icon} color={item.color} size="lg" />
+              <Text>{item.title}</Text>
+            </Flex>
+            <Flex
+              justifyContent="justify-start"
+              alignItems="items-baseline"
+              spaceX="space-x-3"
+              truncate={true}
+            >
+              <Metric>{item.metric}</Metric>
+              <Text truncate={true}>from {item.metricPrev}</Text>
+            </Flex>
+            <Flex
+              justifyContent="justify-start"
+              spaceX="space-x-2"
+              marginTop="mt-4"
+            >
+              <BadgeDelta deltaType={item.deltaType} />
+              <Flex
+                justifyContent="justify-start"
+                spaceX="space-x-1"
+                truncate={true}
+              >
+                <Text color={KPIColors[item.deltaType]}>{item.delta}</Text>
+                <Text truncate={true}> to previous month </Text>
+              </Flex>
+            </Flex>
+          </Card>
+        ))}
+      </ColGrid>
+    </div>
+  );
+}
+
+export default KPICards;
---
+++ src/components/KPICards/KPICardsData.tsx
@@ -0,0 +1,60 @@
+import { DeltaType, Color } from "@tremor/react";
+import { HiCash, HiTicket, HiUserGroup, HiShoppingBag } from "react-icons/hi";
+import { IconType } from "react-icons";
+
+type categories = {
+  title: string;
+  metric: string;
+  metricPrev: string;
+  delta: string;
+  deltaType: DeltaType;
+  color: Color;
+  icon: IconType;
+};
+
+export const KPIColors: { [key: string]: Color } = {
+  increase: "emerald",
+  moderateIncrease: "emerald",
+  unchanged: "orange",
+  moderateDecrease: "rose",
+  decrease: "rose",
+};
+
+export const KPICardData: categories[] = [
+  {
+    title: "Sales",
+    metric: "$ 12,699",
+    metricPrev: "$ 9,456",
+    delta: "34.3%",
+    deltaType: "moderateIncrease",
+    color: "indigo",
+    icon: HiTicket,
+  },
+  {
+    title: "Profit",
+    metric: "$ 40,598",
+    metricPrev: "$ 45,564",
+    delta: "10.9%",
+    deltaType: "moderateDecrease",
+    color: "amber",
+    icon: HiCash,
+  },
+  {
+    title: "Customers",
+    metric: "1,072",
+    metricPrev: "856",
+    delta: "25.3%",
+    deltaType: "moderateIncrease",
+    color: "teal",
+    icon: HiUserGroup,
+  },
+  {
+    title: "Product",
+    metric: "3,461",
+    metricPrev: "3,461",
+    delta: "0.00%",
+    deltaType: "unchanged",
+    color: "rose",
+    icon: HiShoppingBag,
+  },
+];
