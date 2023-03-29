8db2cabb7ef3a1d4756cc5603ecab3f2b832327b
john-seredich <seredichjohnalt@gmail.com>
2023-03-29 17:56:43 -0400

    feat: create HighLevelBlocks container to hold new high level blocks

---
+++ src/components/HighLevelBlocks/HighLevelBlocks.tsx
@@ -0,0 +1,11 @@
+import OverviewBlock from "./OverviewBlock/OverviewBlock";
+
+function HighLevelBlocks() {
+  return (
+    <div style={{ margin: "1rem" }}>
+      <OverviewBlock />
+    </div>
+  );
+}
+
+export default HighLevelBlocks;
