7c75007fee346e2df4df3f6318eae4e372f60961
john-seredich <seredichjohnalt@gmail.com>
2023-03-31 14:17:19 -0400

    refactor: change KPICard color to match theme

--- src/components/KPICards/KPICardsData.tsx
+++ src/components/KPICards/KPICardsData.tsx
@@ -27,7 +27,7 @@ export const KPICardData: categories[] = [
     metricPrev: "$ 9,456",
     delta: "34.3%",
     deltaType: "moderateIncrease",
-    color: "indigo",
+    color: "blue",
     icon: HiTicket,
   },
   {
@@ -36,7 +36,7 @@ export const KPICardData: categories[] = [
     metricPrev: "$ 45,564",
     delta: "10.9%",
     deltaType: "moderateDecrease",
-    color: "amber",
+    color: "blue",
     icon: HiCash,
   },
   {
@@ -45,7 +45,7 @@ export const KPICardData: categories[] = [
     metricPrev: "856",
     delta: "25.3%",
     deltaType: "moderateIncrease",
-    color: "teal",
+    color: "blue",
     icon: HiUserGroup,
   },
   {
@@ -54,7 +54,46 @@ export const KPICardData: categories[] = [
     metricPrev: "3,461",
     delta: "0.00%",
     deltaType: "unchanged",
-    color: "rose",
+    color: "blue",
     icon: HiShoppingBag,
   },
 ];
+
+// export const KPICardData: categories[] = [
+//   {
+//     title: "Sales",
+//     metric: "$ 12,699",
+//     metricPrev: "$ 9,456",
+//     delta: "34.3%",
+//     deltaType: "moderateIncrease",
+//     color: "indigo",
+//     icon: HiTicket,
+//   },
+//   {
+//     title: "Profit",
+//     metric: "$ 40,598",
+//     metricPrev: "$ 45,564",
+//     delta: "10.9%",
+//     deltaType: "moderateDecrease",
+//     color: "amber",
+//     icon: HiCash,
+//   },
+//   {
+//     title: "Customers",
+//     metric: "1,072",
+//     metricPrev: "856",
+//     delta: "25.3%",
+//     deltaType: "moderateIncrease",
+//     color: "teal",
+//     icon: HiUserGroup,
+//   },
+//   {
+//     title: "Product",
+//     metric: "3,461",
+//     metricPrev: "3,461",
+//     delta: "0.00%",
+//     deltaType: "unchanged",
+//     color: "rose",
+//     icon: HiShoppingBag,
+//   },
+// ];
