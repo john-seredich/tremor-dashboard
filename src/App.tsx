e2fa9c95699a3047dd9edc24d10fec30e0c216f8
john-seredich <seredichjohnalt@gmail.com>
2023-03-31 14:18:41 -0400

    feat: add container component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
 import "@tremor/react/dist/esm/tremor.css";
 import styles from "./App.module.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Container from "./components/Container/Container";
 
 function App() {
-  const [activeMenu, setActiveMenu] = useState(false);
-
   return (
     <div className={styles.app}>
-      {activeMenu && <Sidebar />}
-      <div className={styles.app__container}>
-        <Navbar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
+      <Container>
         <Dashboard />
-      </div>
+      </Container>
     </div>
   );
 }
