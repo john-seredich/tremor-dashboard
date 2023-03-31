7cfe2a0d096d7ed7f2e26d9c0cc748b72e5a5665
john-seredich <seredichjohnalt@gmail.com>
2023-03-31 14:19:23 -0400

    feat: add PageHeader into Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
 import KPICards from "../KPICards/KPICards";
+import PageHeader from "../PageHeader/PageHeader";
 import styles from "./Dashboard.module.scss";
 
 function Dashboard() {
   return (
     <div className={styles.dashboard}>
+      <PageHeader title="Dashboard" content="Welcome to your dashboard!" />
       <KPICards />
     </div>
   );
