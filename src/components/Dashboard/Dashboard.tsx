3d43865eef2dcad9f9e086aea55b47487aeadbd2
john-seredich <seredichjohnalt@gmail.com>
2023-04-10 15:35:05 -0400

    feat: create grid layout for dash components

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,30 @@
 import KPICards from "../KPICards/KPICards";
 import PageHeader from "../../Layout/PageHeader/PageHeader";
 import styles from "./Dashboard.module.scss";
+import DashAreaChart from "./DashAreaChart/DashAreaChart";
+import DashTransaction from "./DashTransaction/DashTransaction";
+import { ColGrid } from "@tremor/react";
+import DashSocialBar from "./DashSocialBar/DashSocialBar";
 
 function Dashboard() {
   return (
     <div className={styles.dashboard}>
       <PageHeader title="Dashboard" content="Welcome to your dashboard!" />
       <div className={styles.dashboard__container}>
-        <KPICards />
+        <ColGrid numColsSm={1} numColsLg={1} gapX="gap-x-6" gapY="gap-y-6">
+          <KPICards />
+          <ColGrid
+            numColsSm={1}
+            numColsMd={2}
+            numColsLg={3}
+            gapX="gap-x-4"
+            gapY="gap-y-6"
+          >
+            <DashAreaChart />
+            <DashTransaction />
+            <DashSocialBar />
+          </ColGrid>
+        </ColGrid>
       </div>
     </div>
   );
