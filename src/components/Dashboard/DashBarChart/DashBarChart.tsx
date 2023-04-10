33ddc7ff63207890581c8edc61b405e07683c253
john-seredich <seredichjohnalt@gmail.com>
2023-04-10 15:32:48 -0400

    feat: create DashBarChart component for dashboard

---
+++ src/components/Dashboard/DashBarChart/DashBarChart.tsx
@@ -0,0 +1,27 @@
+import { Card, Title, BarChart, Text } from "@tremor/react";
+import { DashBarChartData } from "./DashBarChartData";
+
+const valueFormatter = (number: number) =>
+  Intl.NumberFormat("us").format(number).toString();
+
+function DashBarChart() {
+  return (
+    <Card>
+      <Title>Ticket Monitoring</Title>
+      <Text>Tickets by Status</Text>
+      <BarChart
+        marginTop="mt-4"
+        height="h-80"
+        data={DashBarChartData}
+        dataKey="Month"
+        categories={["Completed", "In Progress", "Failed"]}
+        colors={["sky", "violet", "fuchsia"]}
+        valueFormatter={valueFormatter}
+        stack={true}
+        relative={true}
+      />
+    </Card>
+  );
+}
+
+export default DashBarChart;
