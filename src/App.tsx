8e62c111079ffbe1c0b66099943f77419dacfa9a
john-seredich <seredichjohnalt@gmail.com>
2023-04-14 13:15:40 -0400

    feat: create browserRouter and router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "@tremor/react/dist/esm/tremor.css";
 import styles from "./App.module.scss";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Container from "./Layout/Container/Container";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes/routes";
 
 function App() {
+  const router = createBrowserRouter(routes);
+
   return (
     <div className={styles.app}>
-      <Container>
-        <Dashboard />
-      </Container>
+      <RouterProvider router={router} />
     </div>
   );
 }
