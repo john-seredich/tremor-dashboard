a6e65f382f37667b03a1049800ef6e21a8280509
john-seredich <seredichjohnalt@gmail.com>
2023-04-26 12:51:02 -0400

    feat: import hashRouter into App when running production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "@tremor/react/dist/esm/tremor.css";
 import styles from "./App.module.scss";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Container from "./Layout/Container/Container";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 import { routes } from "./routes/routes";
 
 function App() {
