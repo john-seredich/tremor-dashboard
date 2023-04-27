322e68aa70cee949f4aeb51196ce110eee883294
john-seredich <seredichjohnalt@gmail.com>
2023-04-27 15:26:15 -0400

    feat: theme options and theme color selectors

--- src/components/ThemeModal/ThemeModal.tsx
+++ src/components/ThemeModal/ThemeModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./ThemeModal.module.scss";
 import { IoCloseSharp } from "react-icons/io5";
 
@@ -5,8 +6,12 @@ type Props = {
   setThemeModalToggle: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+// In the future use context and change styles dynamically
+const themeColors = ["#3b82f6", "#ef4444 ", "#22c55e ", "#a855f7 ", "#f97316 "];
+
 function ThemeModal({ setThemeModalToggle }: Props) {
   const handleThemeToggle = () => setThemeModalToggle(false);
+  const [themeColor, setThemeColor] = useState("#3b82f6");
 
   return (
     <div className={styles.themeModal}>
@@ -15,6 +20,37 @@ function ThemeModal({ setThemeModalToggle }: Props) {
         <button type="button" onClick={handleThemeToggle}>
           <IoCloseSharp />
         </button>
+      </div>
+
+      <div className={styles.themeModal__options}>
+        {/* Add logic to actually detect system setting, and change the theme accordingly  */}
+        <h3>Theme Options</h3>
+        <input type="radio" id="dark" name="theme-options" />
+        <label htmlFor="dark">Dark</label>
+
+        <input type="radio" id="light" name="theme-options" />
+        <label htmlFor="light">Light</label>
+
+        <input type="radio" id="system" name="theme-options" checked />
+        <label htmlFor="system">System</label>
+      </div>
+
+      <div className={styles.themeModal__colors}>
+        <h3>Theme Colors</h3>
+        <div className={styles.themeModal__colors__buttons}>
+          {themeColors.map((button) => {
+            return (
+              <button
+                className={themeColor === button ? styles.active : ""}
+                key={button}
+                type="button"
+                style={{ background: button }}
+                onClick={() => setThemeColor(button)}
+              ></button>
+            );
+          })}
+          <button type="button">Custom Color</button>
+        </div>
       </div>
     </div>
   );
